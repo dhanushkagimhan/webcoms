0b2e25c5a243a75793cc70c34bf255ccbdaa7ac5
Sasini <63692107+Sasini2020@users.noreply.github.com>
2020-11-13 20:47:55 +0530

    Added a responsive navbar and a footer

--- actors/trackChair/trackchairhomepage.php
+++ actors/trackChair/trackchairhomepage.php
@@ -7,46 +7,61 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Home Page</title>
-<link rel="stylesheet" href="../../css/main_style.css">
+    <title>Track chair Home</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="../../css/nav_footer_styles.css">
+
+    <style> 
+input[type=button], input[type=submit] {
+  background-color: #C39BD3;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-decoration: none;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
 </head>
-
-<body style="background-color:#bdc3c7">
-	
-
+<body>
 <nav>
+  <div class="logo">Web-COMS</div>
+          <input type="checkbox" id="click">
+                <label for="click" class="menu-btn">
+                  <i class="fas fa-bars"></i>
+                </label>
     <ul>
+      <li><a class="active" href="trackchairhomepage.php">Home</a></li>
+      <li><a href="#">Conference List</a></li>
       <li><a href="firstround.php">First-round paper evaluation</a></li>
-      <li><a href="paperaccept.php">Paper Acceptance</a></li>
-      <li><a href="assignreviewrs.php">Assign Reviewers </a></li>
-      
-     
+      <li><a href="paperaccept.php">Paper acceptance</a></li>
+      <!-- <li><a href="viewcamerareadycopies.php">View camera ready copies</a></li> -->
+
+
     </ul>
   </nav>
 
   <br><br>
 
-	<div id="main-wrapper">
-		<center>
-			<h2>Track Chair Home Page</h2>
-			<h3>Welcome
-				
-			</h3>
-			<img src="../../imgs/webc.png" class="avatar"/>
-		</center>
-		
-		<form class="myform" action="trackchairhomepage.php" method="post">
-			<input name="logout" type="submit" id="logout_btn" value="Log Out"/><br>
-			
-		</form>
-		
-		<?php
-			if(isset($_POST['logout']))
-			{
-				session_destroy();
-				header('location:../../index.php');
-			}
-		?>
-	</div>
+    <p> Welcome Track-chair </p><br><br>
+
+    <div>
+        <form action="trackchairhomepage.php" method="post">
+            <input type="submit" name="logout" value="Log out" />
+        </form>
+
+        <?php
+            if(isset($_POST['logout'])){
+                session_destroy();
+                header('location:../../index.php');
+            }
+        ?>
+    </div>  
+
+            <!-- Footer section -->
+         <div class="footer">
+            <p>&copy;2020, All rights reserved by www.WebComs.lk</p>
+         </div>
 </body>
+
 </html>
